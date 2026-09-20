5d671fd906f61ac82e56d45fdecb116a215de5c4
ibrahemMohsen <ibrahemmohsen851@gmail.com>
2026-09-20 05:30:21 +0300

    Part02_Initial

---
+++ SmartDeliveryManagementSystem/DeliveryCenter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartDeliveryManagementSystem
+{
+    class DeliveryCenter
+    {
+        private Shipment[] _shipments;
+
+        public DeliveryCenter()
+        {
+            _shipments = new Shipment[10];
+        }
+
+        public Shipment this[int index]
+        {
+            get
+            {
+                if (_shipments is not null &&
+                    index >= 0 &&
+                    index < _shipments.Length)
+                {
+                    return _shipments[index];
+                }
+
+                return default;
+            }
+
+            set
+            {
+                if (_shipments is not null &&
+                    index >= 0 &&
+                    index < _shipments.Length)
+                {
+                    _shipments[index] = value;
+                }
+            }
+        }
+
+        public Shipment this[string trackingCode]
+        {
+            get
+            {
+                if (_shipments is null ||
+                    string.IsNullOrWhiteSpace(trackingCode))
+                {
+                    return default;
+                }
+
+                foreach (Shipment shipment in _shipments)
+                {
+                    if (shipment.TrackingCode == trackingCode)
+                        return shipment;
+                }
+
+                return default;
+            }
+        }
+
+        public bool AddShipment(Shipment shipment)
+        {
+            _shipments ??= new Shipment[10];
+
+            for (int i = 0; i < _shipments.Length; i++)
+            {
+                if (string.IsNullOrWhiteSpace(
+                        _shipments[i].TrackingCode))
+                {
+                    _shipments[i] = shipment;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+}
---
+++ SmartDeliveryManagementSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartDeliveryManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
